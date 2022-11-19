4c67695cd72652ab1437f594ea1081cc37c193b7
Khamthalar5566 <tikkhamthalar@gmail.com>
2022-11-19 17:06:21 +0700

    last update

---
+++ assets/js/custom_js/class_member.js
@@ -0,0 +1,3 @@
+function course_selected(value){
+    window.location.href="template?page=class_member&course_id="+value;
+}
--- pages/class_member.php
+++ pages/class_member.php
@@ -1,7 +1,12 @@
 <?php
-  require_once "controller/class_member_controller.php";
+require_once "controller/class_member_controller.php";
+$school_year = date("Y") . "-" . (date("Y") + 1);
+$course = load_course();
+$course_data = $course->fetchAll(PDO::FETCH_ASSOC);
+$course_id = isset($_GET["course_id"])?$_GET["course_id"]:(($course->rowCount()>0)?$course_data[0]['course_id']:0);
 ?>
 <link rel="stylesheet" href="assets/css/classroom-style.css">
+<link rel="stylesheet" href="assets/css/class-member-style.css">
 <script src="assets/js/jquery-1.9.1.min.js"></script>
 <div class="header-page">
   <div class="page-title">
@@ -14,40 +19,33 @@
       <div class="col-lg-12 grid-margin stretch-card">
         <div class="cus-card card">
           <div class="card-body">
-            <h4 class="card-title notosans">ຈັດຫ້ອງຮຽນ, ສົກຮຽນ <?= date("Y") . "-" . (date("Y") + 1) ?></h4>
+            <h4 class="card-title notosans">ຈັດຫ້ອງຮຽນ, ສົກຮຽນ <?= $school_year ?></h4>
             <div class="top-contents">
-              <form action="" method="POST">
-                <div style="padding-left:10px;padding-right:10px;" class="filter row">
-                  <div style="padding-left:5px;padding-right:5px;" class="col-lg-4 col-sm-6 mb-2">
-                    <select class="form-select notosans" aria-label="Default select" name="cb_course" id="cb_course">
-                    </select>
-                  </div>
-                  <div style="padding-left:5px;padding-right:5px;" class="col-lg-2 col-sm-2 mb-2">
-                    <select class="form-select notosans" aria-label="Default select" name="year_no" id="year_no">
-                    </select>
-                  </div>
-                  <div style="padding-left:5px;padding-right:5px;" class="col-lg-3 col-sm-4 mb-2">
-                    <select class="form-select notosans" aria-label="Default select" name="cb_class" id="cb_class">
-                    </select>
-                  </div>
-                  <div style="padding-left:5px;padding-right:5px;" class="col-lg-3 col-sm-4 mb-2">
-                    <select class="form-select notosans" aria-label="Default select" name="status" id="status">
-                    </select>
-                  </div>
-                  <div style="padding-left:5px;padding-right:5px;" class="col-lg-3 col-sm-4 mb-2">
-                    <input name="txt_search" style="padding: .375rem 2.25rem .375rem .75rem !important;line-height:1.5 !important; height:38px !important;" id="search" type="text" class="form-control notosans" placeholder="Search..." />
-                  </div>
-                  <div style="padding-left:5px;padding-right:5px;" class="col-lg-2">
-                    <button name="search" type="submit" class="btn-newclass btn btn-primary btn-icon-text none-select none-outline notosans">
-                      <i class="ti-search btn-icon-prepend"></i> ຄົ້ນຫາ
-                    </button>
-                  </div>
+              <div style="padding-left:10px;padding-right:10px;" class="filter row">
+                <div style="padding-left:5px;padding-right:5px;" class="col-lg-4 col-sm-6 mb-2">
+                  <select onChange="course_selected(this.value)" class="form-select notosans" aria-label="Default select" name="cb_course" id="cb_course">
+                    <?php
+                    foreach ($course_data as $course) {
+                      $checked = "";
+                      if ($course['course_id'] == $course_id) {
+                        $checked = "selected";
+                      }
+                    ?>
+                      <option value="<?= $course['course_id'] ?>" <?= $checked ?> <?= ($course_id == $course['course_id']) ? "selected" : "" ?>>
+                        <?= $course['course_des'] . "-" . $course['scheme_des'] ?>
+                      </option>
+                    <?php
+                    }
+                    ?>
+                  </select>
                 </div>
-              </form>
+              </div>
             </div>
-            <div class="table-responsive">
-              
-            </div>
+            <section class="body-content">
+              <section class="classroom">
+
+              </section>
+            </section>
           </div>
         </div>
       </div>
@@ -57,3 +55,4 @@
 <?php
 include_once("modals/confirm_dialog.php");
 ?>
+<script src="assets/js/custom_js/class_member.js"></script>
